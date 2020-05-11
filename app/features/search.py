f55c5e40b539ac343169dbb5b22253ddd0849ed1
giltiaeva <angultiaev@gmail.com>
2020-05-11 22:02:02 +0300

    [RF] Comments + fix title

--- app/features/search.py
+++ app/features/search.py
@@ -11,10 +11,30 @@ stemmer = SnowballStemmer('russian')
 
 @lru_cache(maxsize=100000)
 def stem_word(word):
+    """
+    Стеммировать отдельное слово
+
+    :param word: Слово для стемминга
+    :type word: str
+
+    :return: Основа слова
+    :rtype: str
+    """
+
     return stemmer.stem(word)
 
 
 def stem_text(text):
+    """
+    Стеммировать все слова в тексте
+
+    :param text: Текст для обработки
+    :type text: str
+
+    :return: Список из основ всех слов в тексте в порядке следования
+    :rtype: list
+    """
+
     with Pool(processes=cpu_count()) as pool:
         result = pool.map(stem_word,
                           [word for word in re.findall(r'[a-zа-яё]+',
@@ -24,11 +44,42 @@ def stem_text(text):
 
 
 @lru_cache(maxsize=100000)
-def calculate_distance(word1, word2):
+def levenshtein_distance(word1, word2):
+    """
+    Вычислить расстояние Левенштекна для двух слов
+
+    :return: Рассточние Левенштейна
+    :rtype: int
+    """
+
     return distance(word1, word2)
 
 
 def full_text_search(search_request, text, metric='levenshtein', n_pad_words=5, exact=True):
+    """
+    Полнотекстовый поиск по тексту
+
+    :param search_request: Поисковый запрос
+    :type search_request: str
+    :param text: Текст, в котором осуществляется поиск
+    :type text: str
+    :param metric: Метрика рассчета близости слов default=levenshtein
+    :type metric: str
+    :param n_pad_words: Количество слов,
+                        которое необходимо вернуть до и после найденного фрагмента
+    :type n_pad_words: int
+    :param exact: Флаг точного совпадения
+    :type exact: bool
+
+    :return: Найденный фрагмент текста с указанным количеством слов до и после,
+             если такой фрагмент найден, и None -- если не найден
+    :rtype: str
+    """
+
+    assert metric in ['levenshtein']
+    if metric == 'levenshtein':
+        calculate_distance = levenshtein_distance
+
     accuracy = int(not exact)
     search_request = search_request.strip()
     if not search_request:
@@ -73,6 +124,25 @@ def full_text_search(search_request, text, metric='levenshtein', n_pad_words=5, 
 
 
 def file_search(pattern, year, metric='levenshtein', n_pad_words=5, exact=True):
+    """
+    Функция-обертка. Полнотекстовый поиск по файлу отчета
+
+    :param pattern: Поисковый запрос
+    :type pattern: str
+    :param year: Год отчета
+    :type year: str/int
+    :param metric: Метрика рассчета близости слов default=levenshtein
+    :type metric: str
+    :param n_pad_words: Количество слов,
+                        которое необходимо вернуть до и после найденного фрагмента
+    :type n_pad_words: int
+    :param exact: Флаг точного совпадения
+    :type exact: bool
+
+    :return: Результат выполнения понотекстового поиска по отчету
+    :rtype: str
+    """
+
     fname = f'app/static/data/txt/CBR_report{year}.txt'
     with open(fname, encoding='utf8') as f:
         data = f.read()
